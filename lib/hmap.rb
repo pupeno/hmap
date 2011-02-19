2d985c0b42ed4b3a5e1331185931c8794b6fb39b
J. Pablo Fernández <pupeno@pupeno.com>
2011-02-19 20:07:58 +0100

    Fixed some issues and added more tests.

--- lib/hmap.rb
+++ lib/hmap.rb
@@ -6,10 +6,11 @@ class Hash
       if pair.is_a? Hash
         new_hash.merge(pair)
       elsif pair.is_a? Array
-        if pair.length < 2
-          raise "When using hmap, when you return arrays, they must have at least two elements; #{pair} has only one."
+        if pair.length != 2
+          raise "When using hmap, when you return arrays, they must have two and only two elements; #{pair} doesn't"
         end
-        new_hash[pair[0]] = pair[1..-1]
+        new_hash[pair[0]] = pair[1]
+        new_hash
       else
         raise "When using hmap you need to return arrays or hashes, #{pair} isn't either."
       end
--- test/hmap_test.rb
+++ test/hmap_test.rb
@@ -8,9 +8,28 @@ class HashTest < Test::Unit::TestCase
     assert_equal({}, {}.hmap { |a, b| [a, b] })
   end
 
-  def test_raises_when_invalid
+  def test_raises_when_totally_invalid
     assert_raises RuntimeError do
-      {:a => :b}.hmap { |_, _| "hello world" }
+      {:a => :b}.hmap { |a, _| a }
     end
+  end
+
+  def test_raises_when_invalid_array
+    assert_raises RuntimeError do
+      {:a => :b}.hmap { |a, _| [a] }
+    end
+    assert_raises RuntimeError do
+      {:a => :b}.hmap { |a, b| [a, b, b] }
+    end
+  end
+
+  def test_identity_with_hash
+    hash = {:a => :b, :c => :d, :e => :f}
+    assert_equal(hash, hash.hmap { |a, b| {a => b} })
+  end
+
+  def test_identity_with_array
+    hash = {:a => :b, :c => :d, :e => :f}
+    assert_equal(hash, hash.hmap { |a, b| [a, b] })
   end
 end
